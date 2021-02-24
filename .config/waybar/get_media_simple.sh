3f43f875fc81cdefcb88a92ba1c4c4005f3db744
HansBlackCat <jeremy3599@snu.ac.kr>
2021-02-24 13:14:19 +0900

    Simple Wayland config

---
+++ .config/waybar/get_media_simple.sh
@@ -0,0 +1,14 @@
+media=$(playerctl metadata -f "{{title}} - {{artist}}")
+player_status=$(playerctl status)
+
+if [[ $player_status = "Playing" ]]
+then
+    song_status=''
+elif [[ $player_status = "Paused" ]]
+then
+    song_status=''
+else
+    song_status='Music stopped'
+fi
+
+echo -e "$song_status $media"
