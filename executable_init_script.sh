12df98bb3cf1a0320f27cd825436fe067ccbe7fe
HansBlackCat <jeremy3599@snu.ac.kr>
2022-09-20 22:14:46 +0900

    seperate init_script

--- executable_init_script.sh
+++ executable_init_script.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 sudo apt update
 # add neovim > 0.5 repos
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python3-dev python3-pip
+sudo apt-get install neovim -y
+sudo apt-get install python-dev python3-dev python3-pip -y
 
-sudo apt install gcc python3 python-dev python3-dev tmux cmake -y
+sudo apt install gcc python3 python-dev python3-dev tmux cmake exuberant-ctags cscope -y
 sudo apt install git openssh-server build-essential ncurses-dev libssl-dev flex bison libelf-dev neovim zsh fzf -y
 sudo apt install flex -y
 
@@ -18,30 +18,3 @@ sudo systemctl enable --now ssh
 echo "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Install zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Install zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Install powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-echo "chsh"
-sudo chsh -s $(which zsh)
-
-# neovim
-# Vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# YCM
-mkdir -p ${HOME}/.config/nvim/bundle
-cd ${HOME}/.config/nvim/bundle
-git clone https://github.com/ycm-core/YouCompleteMe.git
-cd YouCompleteMe
-git submodule update --init --recursive
-python3 install.py --clangd-completer
-
-# run :Pluginstall
-vim +'PlugInstall --sync' +qa
