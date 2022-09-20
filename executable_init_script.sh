78b21d206e129b4589dc031e94dd11d1aafc25dc
HansBlackCat <jeremy3599@snu.ac.kr>
2022-09-20 14:48:31 +0900

    init_script

--- executable_init_script.sh
+++ executable_init_script.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 sudo apt update
-sudo apt install git build-essential ncurses-dev libssl-dev flex bison libelf-dev neovim zsh fzf -y
+sudo apt install git openssh-server build-essential ncurses-dev libssl-dev flex bison libelf-dev neovim zsh fzf -y
+sudo apt install flex -y
 
+# Enable openssh-server
+sudo systemctl enable --now ssh
+
+# zsh
+echo "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Install zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Install zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# neovim
+# Vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+# run :Pluginstall
+vim +'PlugInstall --sync' +qa
