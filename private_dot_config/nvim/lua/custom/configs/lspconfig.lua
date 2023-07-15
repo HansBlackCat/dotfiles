47f0da30437c3a6f7ec48c626604744e4145129f
HansBlackCat <jeremy3599@snu.ac.kr>
2023-07-15 15:09:41 +0900

    nvim new

--- private_dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,17 +2,54 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd"}
+local util = require "lspconfig/util"
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls" },
+  cmd_env = {
+    GOFLAGS = "-tags=test,e2e_test,integration_test,acceptance_test",
+  },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
 
--- Without the loop, you would have to manually set up each LSP 
--- 
+-- Collision with rust-tools
+-- lspconfig.rust_analyzer.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = { "rust" },
+--   root_dir = util.root_pattern "Cargo.toml",
+--   settings = {
+--     ["rust_analyzer"] = {
+--       cargo = {
+--         allFeatures = true,
+--       },
+--     },
+--   },
+-- }
+
+-- local servers = { "html", "cssls", "clangd"}
+--
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--   }
+-- end
+
+-- Without the loop, you would have to manually set up each LSP
+--
 -- lspconfig.html.setup {
 --   on_attach = on_attach,
 --   capabilities = capabilities,
@@ -22,4 +59,3 @@ end
 --   on_attach = on_attach,
 --   capabilities = capabilities,
 -- }
-
--- private_dot_config/nvim/lua/custom/plugins.lua
+++ private_dot_config/nvim/lua/custom/plugins.lua
@@ -1,14 +1,19 @@
 local plugins = {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
         "vim",
         "lua",
         "c",
-        "cpp"
-      }
-    }
+        "cpp",
+        "go",
+      },
+    },
   },
   {
     "williamboman/mason.nvim",
@@ -16,24 +21,93 @@ local plugins = {
       ensure_installed = {
         "lua-language-server",
         "prettier",
+        "shellcheck",
+        -- lua
+        "lua-language-server",
         "stylua",
-        "shellcheck"
-      }
-    }
+        -- golang
+        "gopls",
+        "golines",
+        "goimports-reviser",
+        "delve",
+        -- rust
+        "rust-analyzer",
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
 
-   dependencies = {
-     "jose-elias-alvarez/null-ls.nvim",
-     config = function()
-       require "custom.configs.null-ls"
-     end,
-   },
-   config = function()
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+    config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-   end,
-  }
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings "dap"
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings "dap_go"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.config.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, { name = "crates" })
+      return M
+    end,
+  },
 }
 return plugins
